78e5c43f67a59f3021da531e87fff9b2fec47527
Heinrich Drendorf <risach@rosenhaus.at>
2019-10-22 13:05:29 +0200

    Initial commit

---
+++ src/TestHelloWorld.java
@@ -0,0 +1,37 @@
+public class TestHelloWorld {
+
+    public static void main(String[] args) {
+        // System.out.println("Daten");
+        System.out.println("\nHello World!");
+        test();
+        TestWindow fenster = new TestWindow();
+        fenster.setVisible( true );
+        fenster.setSize( 200, 120 );
+        fenster.setLocation( 200,120 );
+    }
+
+    public static void test (){
+        // @TODO: Extend this method...
+        /*
+
+        Unterschiede von Datentypen, wenn man 3,5 / dreieinhalb Äpfel definieren möchte.
+
+         */
+
+        String aepfel = "Dreieinhalb";
+        String birnen = "3.5";
+
+        String obst = aepfel + birnen;
+
+        System.out.println(obst);
+
+        int aepfel1 = 3;
+
+        double aepfel2 = 3.5;
+        double birnen2 = 3.5;
+
+        System.out.println(aepfel2 + birnen2);
+
+    }
+
+}
---
+++ src/TestWindow.java
@@ -0,0 +1,5 @@
+import javax.swing.*;
+
+public class TestWindow extends JFrame{
+    private JButton button1;
+}
