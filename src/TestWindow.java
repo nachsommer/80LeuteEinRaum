2e1d7cedb8c740a76b454977331014458f07b703
Heinrich Drendorf <risach@rosenhaus.at>
2019-11-26 17:43:38 +0100

    Eine Hausaufgabe...

--- src/TestWindow.java
+++ src/TestWindow.java
@@ -7,10 +7,32 @@ public class TestWindow extends JFrame {
     private JButton button1;
     private JPanel jPanel0;
     private JPanel jPanel1;
+    private JTextField textField1;
+    private JTextField textField2;
 
 
     public TestWindow() {
         this.add( jPanel0 );
+        button1.addActionListener( new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println( "Knopf gedrückt!" );
+
+            }
+        } );
+        textField1.addActionListener( new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println( "Wert eingegeben! Er lautet: " + textField1.getText() );
+                /*
+
+                An dieser Stelle benötigen wir eine Methode, die es erlaubt, den Eingabewert des textField1 in eine Integer-Variable umzuwandeln.
+                Hausaufgabe!
+
+                 */
+
+            }
+        } );
     }
 
 }
